19d227bd30510854cd50786cc40002cdb2ed0bd2
AngusJK <anguskellett@hotmail.com>
2015-04-09 00:02:28 +0000

    implements everything

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,42 @@
 # Homepage (Root path)
+require 'sinatra/json'
+
 get '/' do
   erb :index
+end
+
+get '/index' do
+  erb :index
+end
+
+get '/list' do
+  @contact = Contact.all
+  erb :list
+end
+
+get '/contacts' do
+  contacts = Contact.all
+  content_type :json
+  contacts.to_json
+end
+
+get '/search' do
+  @input = '%' + params[:input] + '%'
+
+  @result = Contact.where('first_name like ?', @input)
+
+  json @result
+end
+
+post '/create' do
+  @first_name = params[:first_name]
+  @last_name = params[:last_name]
+  @email = params[:email]
+  @phone = params[:phone]
+
+  Contact.create(:first_name => @first_name,
+              :last_name => @last_name,
+              :email => @email,
+              :phone => @phone)
+  
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,52 @@
 $(document).ready(function() {
+//   $( "#target" ).submit(function( event ) {
+//   event.preventDefault();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+// });
+//   $( "#other" ).click(function() {
+//   $( "#target" ).submit();
+// });
+  
+
+  $("#Submit").on('click', function(){
+
+    first_name=$('#firstname').val();
+    last_name=$('#lastname').val();
+    email=$('#email').val();
+    phone=$('#phone').val();
+    // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    
+      postParams = {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      phone: phone
+      };
+
+    $.ajax({
+      url: "/create",
+      method: 'POST',
+      data: postParams});
+  });
+
+  $('#Search').on('click', function(){
+
+    input=$('#input').val();
+    var params = {input: input}
+
+    $.ajax({
+      url: "/search",
+      method: 'GET',
+      data: params}).done(function(result){
+        console.log("inside done");
+        $('#result_first_name').replaceWith( '<h4 id="result_first_name">' + result[0].first_name + "</h4>");  
+        $('#result_last_name').replaceWith( '<h4 id="result_last_name">' + result[0].last_name + "</h4>"); 
+        $('#result_email').replaceWith( '<h4 id="result_email">' + result[0].email + "</h4>"); 
+        $('#result_phone').replaceWith( '<h4 id="result_phone">' + result[0].phone + "</h4>"); 
+      });
+    });
+
+  });
+
+
+
